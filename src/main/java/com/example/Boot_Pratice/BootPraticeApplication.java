5b7a46a659d1bb3ad5fc9e0287d689790b53e706
Hp <Hp@DESKTOP-HVQMAG2>
2024-09-18 15:46:18 +0530

    Checkout

---
+++ src/main/java/com/example/Boot_Pratice/BootPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Boot_Pratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootPraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootPraticeApplication.class, args);
+	}
+
+}
